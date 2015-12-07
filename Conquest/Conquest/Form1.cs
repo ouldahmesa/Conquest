7a8ed63b0d7eca015cfde313d42268169f352479
ouldahmesa <ouldahmesa@etml.educanet2.ch>
2015-12-07 12:01:52 +0100

    test

--- Conquest/Conquest/Form1.cs
+++ Conquest/Conquest/Form1.cs
@@ -16,5 +16,10 @@ namespace Conquest
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*testdsfdfdsfdff*/
+        }
     }
 }
