72e2a1d88b3a17b2805300b4890ae1334178f429
ouldahmesa <ouldahmesa@etml.educanet2.ch>
2015-12-09 12:35:18 +0100

    tete

--- Conquest/Conquest/Boutique.cs
+++ Conquest/Conquest/Boutique.cs
@@ -19,6 +19,7 @@ namespace Conquest
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
         }
     }
 }
--- Conquest/Conquest/Form1.Designer.cs
+++ Conquest/Conquest/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1128, 539);
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "Form1";
